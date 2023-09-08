46a4fdc280f781a5d8b97c8bd63739c3e868688b
Ericson Gabuya <ericsongabuya20@gmail.com>
2023-09-08 18:57:36 +0800

    Created index.html file

---
+++ index.js
@@ -0,0 +1,456 @@
+  const cards1 = document.getElementById('cards-1');
+  const xbtn1 = document.getElementById('xbtn1');
+  const cards2 = document.getElementById('cards-2');
+  const xbtn2 = document.getElementById('xbtn2');
+  const cards3 = document.getElementById('cards-3');
+  const xbtn3 = document.getElementById('xbtn3');
+  const cards4 = document.getElementById('cards-4');
+  const xbtn4 = document.getElementById('xbtn4');
+  const cards5 = document.getElementById('cards-5');
+  const xbtn5 = document.getElementById('xbtn5');
+  const cards6 = document.getElementById('cards-6');
+  const xbtn6 = document.getElementById('xbtn6');
+  const cards7 = document.getElementById('cards-7');
+  const xbtn7 = document.getElementById('xbtn7');
+  const cards8 = document.getElementById('cards-8');
+  const xbtn8 = document.getElementById('xbtn8');
+  const cards9 = document.getElementById('cards-9');
+  const xbtn9 = document.getElementById('xbtn9');
+
+  xbtn1.addEventListener('click', () => {
+    cards1.remove();
+  });
+  xbtn2.addEventListener('click', () => {
+    cards2.remove();
+  });
+  xbtn3.addEventListener('click', () => {
+    cards3.remove();
+  });
+  xbtn4.addEventListener('click', () => {
+    cards4.remove();
+  });
+  xbtn5.addEventListener('click', () => {
+    cards5.remove();
+  });
+  xbtn6.addEventListener('click', () => {
+    cards6.remove();
+  });
+  xbtn7.addEventListener('click', () => {
+    cards7.remove();
+  });
+  xbtn8.addEventListener('click', () => {
+    cards8.remove();
+  });
+  xbtn9.addEventListener('click', () => {
+    cards9.remove();
+  });
+
+const checked =document.getElementById("check");
+const images = document.querySelector(".image");
+const xSymbol = document.querySelector(".x-symbol");
+const textPosition = document.querySelector(".text-position");
+const disableimg = images.addEventListener('mouseover', function(){
+  images.style.height = "220px";
+  xSymbol.style.bottom = "228px";
+  xSymbol.style.display = "inline-block";
+  xSymbol.style.display = "flex";
+  textPosition.style.bottom = "56px";
+  checked.style.display = "block";
+  checked.style.bottom = "25px";
+  checked.style.display = "flex";
+    
+});
+
+const iconHide = document.getElementById("iconhide")
+  function heartColor() {
+    if(iconHide.style.display === "block") {
+      iconHide.style.display = "none";
+    }else {
+      iconHide.style.display = "block";
+    }
+  };
+
+const checked1 =document.getElementById("check1");
+const images1 = document.querySelector(".image-2");
+const xSymbol1 = document.getElementById("xbtn2");
+const textPosition1 = document.querySelector(".text-position-1");
+images1.addEventListener('mouseover', function(){
+  images1.style.height = "228px";
+  xSymbol1.style.bottom = "237px";
+  xSymbol1.style.display = "inline-block";
+  xSymbol1.style.display = "flex";
+  textPosition1.style.top = "7px";
+  checked1.style.display = "block";
+  checked1.style.bottom = "13px";
+  checked1.style.display = "flex";
+});
+
+  const iconHide1 = document.getElementById("iconhide1")
+  function heartColor1() {
+    if(iconHide1.style.display === "block") {
+      iconHide1.style.display = "none";
+    }else {
+      iconHide1.style.display = "block";
+    }
+  };
+const checked2 =document.getElementById("check2");
+const images2 = document.querySelector(".image-3");
+const xSymbol2 = document.getElementById("xbtn3");
+const textPosition2 = document.querySelector(".text-position-22");
+images2.addEventListener('mouseover', function(){
+  images2.style.height = "197px";
+  xSymbol2.style.bottom = "205px";
+  xSymbol2.style.display = "inline-block";
+  xSymbol2.style.display = "flex";
+  textPosition2.style.top = "40px";
+  checked2.style.display = "block";
+  checked2.style.top = "16px";
+  checked2.style.display = "flex";
+});
+  const iconHide2 = document.getElementById("iconhide2")
+  function heartColor2() {
+    if(iconHide2.style.display === "block") {
+      iconHide2.style.display = "none";
+    }else {
+      iconHide2.style.display = "block";
+    }
+  };
+const checked3 =document.getElementById("check3");
+const images3 = document.querySelector(".image-4");
+const xSymbol3 = document.getElementById("xbtn4");
+const textPosition3 = document.querySelector(".text-position-3");
+images3.addEventListener('mouseover', function(){
+  images3.style.height = "180px";
+  xSymbol3.style.bottom = "188px";
+  xSymbol3.style.display = "inline-block";
+  xSymbol3.style.display = "flex";
+  textPosition3.style.top = "29px";
+  checked3.style.display = "block";
+  checked3.style.top = "15px";
+  checked3.style.display = "flex";
+});
+  const iconHide3 = document.getElementById("iconhide3")
+  function heartColor3() {
+    if(iconHide3.style.display === "block") {
+      iconHide3.style.display = "none";
+    }else {
+      iconHide3.style.display = "block";
+    }
+  };
+const checked4 =document.getElementById("check4");
+const images4 = document.querySelector(".image-5");
+const xSymbol4 = document.getElementById("xbtn5");
+const textPosition4 = document.querySelector(".text-position-4");
+images4.addEventListener('mouseover', function(){
+  images4.style.height = "205px";
+  xSymbol4.style.bottom = "213px";
+  xSymbol4.style.display = "inline-block";
+  xSymbol4.style.display = "flex";
+  textPosition4.style.top = "30px";
+  checked4.style.display = "block";
+  checked4.style.top = "15px";
+  checked4.style.display = "flex";
+});
+  const iconHide4 = document.getElementById("iconhide4")
+  function heartColor4() {
+    if(iconHide4.style.display === "block") {
+      iconHide4.style.display = "none";
+    }else {
+      iconHide4.style.display = "block";
+    }
+  };
+const checked5 =document.getElementById("check5");
+const images5 = document.querySelector(".image-6");
+const xSymbol5 = document.getElementById("xbtn6");
+const textPosition5 = document.querySelector(".text-position-5");
+images5.addEventListener('mouseover', function(){
+  images5.style.height = "175px";
+  xSymbol5.style.bottom = "182px";
+  xSymbol5.style.display = "inline-block";
+  xSymbol5.style.display = "flex";
+  textPosition5.style.top= "34px";
+  checked5.style.display = "block";
+  checked5.style.top = "19px";
+  checked5.style.display = "flex";
+});
+  const iconHide5 = document.getElementById("iconhide5")
+  function heartColor5() {
+    if(iconHide5.style.display === "block") {
+      iconHide5.style.display = "none";
+    }else {
+      iconHide5.style.display = "block";
+    }
+  };
+const checked6 =document.getElementById("check6");
+const images6 = document.querySelector(".image-7");
+const xSymbol6 = document.getElementById("xbtn7");
+const textPosition6 = document.querySelector(".text-position-6");
+images6.addEventListener('mouseover', function(){
+  images6.style.height = "227px";
+  xSymbol6.style.bottom = "235px";
+  xSymbol6.style.display = "inline-block";
+  xSymbol6.style.display = "flex";
+  textPosition6.style.top = "33px";
+  checked6.style.display = "block";
+  checked6.style.top = "15px";
+  checked6.style.display = "flex";
+});
+  const iconHide6 = document.getElementById("iconhide6")
+  function heartColor6() {
+    if(iconHide6.style.display === "block") {
+      iconHide6.style.display = "none";
+    }else {
+      iconHide6.style.display = "block";
+    }
+  };
+const checked7 =document.getElementById("check7");
+const images7 = document.querySelector(".image-8");
+const xSymbol7 = document.getElementById("xbtn8");
+const textPosition7 = document.querySelector(".text-position-7");
+images7.addEventListener('mouseover', function(){
+  images7.style.height = "180px";
+  xSymbol7.style.bottom = "188px";
+  xSymbol7.style.display = "inline-block";
+  xSymbol7.style.display = "flex";
+  textPosition7.style.top = "29px";
+  checked7.style.display = "block";
+  checked7.style.top = "15px";
+  checked7.style.display = "flex";
+});
+  const iconHide7 = document.getElementById("iconhide7")
+  function heartColor7() {
+    if(iconHide7.style.display === "block") {
+      iconHide7.style.display = "none";
+    }else {
+      iconHide7.style.display = "block";
+    }
+  };
+const checked8 =document.getElementById("check8");
+const images8 = document.querySelector(".image-9");
+const xSymbol8 = document.getElementById("xbtn9");
+const textPosition8 = document.querySelector(".text-position-8");
+images8.addEventListener('mouseover', function(){
+  images8.style.height = "195px";
+  xSymbol8.style.bottom = "203px";
+  xSymbol8.style.display = "inline-block";
+  xSymbol8.style.display = "flex";
+  textPosition8.style.top = "40px";
+  checked8.style.display = "block";
+  checked8.style.top = "20px";
+  checked8.style.display = "flex";
+});
+  const iconHide8 = document.getElementById("iconhide8")
+  function heartColor8() {
+    if(iconHide8.style.display === "block") {
+      iconHide8.style.display = "none";
+    }else {
+      iconHide8.style.display = "block";
+    }
+  };
+
+    // let disabledFunc = true;
+
+  // function hiddenCheck1() {
+  //   if(checked.style.display === "block") {
+  //     checked.style.display = "none";
+  //     images.style.height = "250px";
+  //     textPosition.style.bottom = "80px";
+  //     xSymbol.style.bottom = "258px";
+  //   }else {
+  //     checked.style.display = "block";
+  //     images.style.height = "220px";
+  //     textPosition.style.bottom = "68px";
+  //     xSymbol.style.bottom = "228px";
+  //   }
+  //   disabledFunc = false;
+  // };
+
+// function color1() {
+//   if (document.getElementById('heart1').classList.contains("favorite")) {
+//     document.getElementById('heart1').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart1').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart1').classList.toggle('favorite');
+
+//   // hiddenCheck2().disabledFunc = true;
+// }
+
+// function color2() {
+//   if (document.getElementById('heart2').classList.contains("favorite")) {
+//     document.getElementById('heart2').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart2').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart2').classList.toggle('favorite');
+
+//   // hiddenCheck3().disabledFunc = true;
+// }
+
+// function color3() {
+//   if (document.getElementById('heart3').classList.contains("favorite")) {
+//     document.getElementById('heart3').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart3').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart3').classList.toggle('favorite');
+
+//   // hiddenCheck4().disabledFunc = true;
+// }
+
+// function color4() {
+//   if (document.getElementById('heart4').classList.contains("favorite")) {
+//     document.getElementById('heart4').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart4').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart4').classList.toggle('favorite');
+
+//   // hiddenCheck5().disabledFunc = true;
+// }
+
+// function color5() {
+//   if (document.getElementById('heart5').classList.contains("favorite")) {
+//     document.getElementById('heart5').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart5').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart5').classList.toggle('favorite');
+
+//   // hiddenCheck6().disabledFunc = true;
+// }
+
+// function color6() {
+//   if (document.getElementById('heart6').classList.contains("favorite")) {
+//     document.getElementById('heart6').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart6').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart6').classList.toggle('favorite');
+
+//   // hiddenCheck7().disabledFunc = true;
+// }
+
+// function color7() {
+//   if (document.getElementById('heart7').classList.contains("favorite")) {
+//     document.getElementById('heart7').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart7').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart7').classList.toggle('favorite');
+
+//   // hiddenCheck8().disabledFunc = true;
+// }
+
+
+// function color8() {
+//   if (document.getElementById('heart8').classList.contains("favorite")) {
+//     document.getElementById('heart8').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart8').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart8').classList.toggle('favorite');
+
+//   // hiddenCheck9().disabledFunc = true;  
+// }
+
+//   function color() {
+//   if (document.getElementById('heart').classList.contains("favorite")) {
+//     document.getElementById('heart').innerHTML = "&#x2661;"
+//   } else {
+//     document.getElementById('heart').innerHTML = "&#10084;"
+//   }
+  
+//   document.getElementById('heart').classList.toggle('favorite');
+//   textPosition.style.top = "-40px";
+//   hiddenCheck1().disabledFunc = true;
+// }
+
+  // function hiddenCheck2() {
+  //   const check2 = document.getElementById("check2");
+
+  //   if(check2.style.display === "block") {
+  //     check2.style.display = "none";
+  //   }else {
+  //     check2.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck3() {
+  //   const check3 = document.getElementById("check3");
+
+  //   if(check3.style.display === "block") {
+  //     check3.style.display = "none";
+  //   }else {
+  //     check3.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck4() {
+  //   const check4 = document.getElementById("check4");
+
+  //   if(check4.style.display === "block") {
+  //     check4.style.display = "none";
+  //   }else {
+  //     check4.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck5() {
+  //   const check5 = document.getElementById("check5");
+
+  //   if(check5.style.display === "block") {
+  //     check5.style.display = "none";
+  //   }else {
+  //     check5.style.display = "block";
+  //   }
+  // }
+  // function hiddenCheck6() {
+  //   const check6 = document.getElementById("check6");
+
+  //   if(check6.style.display === "block") {
+  //     check6.style.display = "none";
+  //   }else {
+  //     check6.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck7() {
+  //   const check7 = document.getElementById("check7");
+
+  //   if(check7.style.display === "block") {
+  //     check7.style.display = "none";
+  //   }else {
+  //     check7.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck8() {
+  //   const check8 = document.getElementById("check8");
+
+  //   if(check8.style.display === "block") {
+  //     check8.style.display = "none";
+  //   }else {
+  //     check8.style.display = "block";
+  //   }
+  // }
+
+  // function hiddenCheck9() {
+  //   const check9 = document.getElementById("check9");
+
+  //   if(check9.style.display === "block") {
+  //     check9.style.display = "none";
+  //   }else {
+  //     check9.style.display = "block";
+  //   }
+  // }
+  
+
